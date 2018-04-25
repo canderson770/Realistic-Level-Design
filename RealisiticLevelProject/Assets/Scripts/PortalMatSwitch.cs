0a427a01bfaee45e641b1bdd8ebdc3322962ca89
Unknown <rp17000@gmail.com>
2018-04-25 12:35:38 -0600

    extras

---
+++ RealisiticLevelProject/Assets/Scripts/PortalMatSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalMatSwitch : MonoBehaviour
+{
+    public Material originalMat;
+    Renderer rend;
+    public Material blackMat;
+    public KeyCode on;
+    public KeyCode off;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+        if(originalMat != null)
+        {
+            originalMat = rend.material;
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(on))
+        {
+            rend.material = originalMat;
+        }
+        else  if(Input.GetKeyDown(off))
+        {
+            rend.material = blackMat;
+        }
+    }
+}
